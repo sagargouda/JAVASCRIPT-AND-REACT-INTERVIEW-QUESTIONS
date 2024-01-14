ed03f30fb41887a9d9a53c410eecf3bb47213c9f
sagar <sagarc2580@gmail.com>
2024-01-14 12:33:39 +0530

    13

--- JAVASCRIPT/tweleve-twenty/index.js
+++ JAVASCRIPT/tweleve-twenty/index.js
@@ -1,6 +1,16 @@
 //!! 12)
 //TODO
-let greeting;
-greetign = {}; // Typo!
-console.log(greetign);
+// let greeting;
+// greetign = {}; // Typo!
+// console.log(greetign);
 //?? ANSWER IS {} , beacuse we created an empty object in global scope which means greetign is assigned as window.greetign = {} , to avoid this we use 'use strict'
+
+//!! 13)
+//Todo
+// function bark() {
+//   console.log("Woof!");
+// }
+
+// bark.animal = "dog";
+
+//?? fucntion are objects only we can do this
