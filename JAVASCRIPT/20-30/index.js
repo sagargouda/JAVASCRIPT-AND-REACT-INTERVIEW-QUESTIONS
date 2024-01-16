4b4627e99abd36ba54247496ed88b23d58c0e3d1
sagar <sagarc2580@gmail.com>
2024-01-16 16:43:53 +0530

    21

---
+++ JAVASCRIPT/20-30/index.js
@@ -0,0 +1,15 @@
+//!! 21
+//todo
+// function checkAge(data) {
+//   if (data === { age: 18 }) {
+//     console.log("You are an adult!");
+//   } else if (data == { age: 18 }) {
+//     console.log("You are still an adult.");
+//   } else {
+//     console.log(`Hmm.. You don't have an age I guess`);
+//   }
+// }
+
+// checkAge({ age: 18 });
+
+//?? Hmm.. You don't have an age I guess , the reason you will understand if you know primitve an dobject types , here primitives compared by their value , objects are compared by their refrence , so the 2 objects we are comparing don't have that , the object we passed as parameter refers to doiffrent location in memory than object we used in order to check their equality
