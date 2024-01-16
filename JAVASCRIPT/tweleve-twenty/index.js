053961bbba8c7ce9e1def4232209993e0fd956d7
sagar <sagarc2580@gmail.com>
2024-01-16 16:14:33 +0530

    14

--- JAVASCRIPT/tweleve-twenty/index.js
+++ JAVASCRIPT/tweleve-twenty/index.js
@@ -14,3 +14,19 @@
 // bark.animal = "dog";
 
 //?? fucntion are objects only we can do this
+
+//!! 14)
+//Todo
+function Person(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+}
+
+const member = new Person("Lydia", "Hallie");
+Person.getFullName = function () {
+  return `${this.firstName} ${this.lastName}`;
+};
+
+console.log(member.getFullName());
+
+//?? in javascript , function are just objects , so method getFullname gets added to constrcutp fucntion object itself . for that reason we can call person.getfullname() and member.getfullname throws typeerror
