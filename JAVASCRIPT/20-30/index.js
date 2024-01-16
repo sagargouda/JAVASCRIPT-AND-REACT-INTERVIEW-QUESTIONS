f2a0710f6eb26b6d7eeb7627d4e70ee472ae0fad
sagar <sagarc2580@gmail.com>
2024-01-16 16:45:21 +0530

    22

--- JAVASCRIPT/20-30/index.js
+++ JAVASCRIPT/20-30/index.js
@@ -13,3 +13,12 @@
 // checkAge({ age: 18 });
 
 //?? Hmm.. You don't have an age I guess , the reason you will understand if you know primitve an dobject types , here primitives compared by their value , objects are compared by their refrence , so the 2 objects we are comparing don't have that , the object we passed as parameter refers to doiffrent location in memory than object we used in order to check their equality
+
+//!! 22
+//todo
+// function getAge(...args) {
+//   console.log(typeof args);
+// }
+
+// getAge(21);
+//?? it is indeed object , you will understand this when you know about argumnents object
