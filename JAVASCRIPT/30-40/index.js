a9e757c38195e6ee4464c01bf31081577f4a3206
sagar <sagarc2580@gmail.com>
2024-01-17 16:37:58 +0530

    32

--- JAVASCRIPT/30-40/index.js
+++ JAVASCRIPT/30-40/index.js
@@ -9,3 +9,15 @@
 // console.log(name.giveSagarPizza());
 
 //?? just give sagar pizza already , so here string is a built in function which i can add properties to , i just added a method to it's prototype priumitve stringsare automatically cvonverted into string object
+
+//!! 32
+//todo
+const a = {};
+const b = { key: "b" };
+const c = { key: "c" };
+
+a[b] = 123;
+a[c] = 456;
+
+console.log(a[b]);
+//?? here object keys are automatically converted to strings so i am setting object key to object a with value 123; when we stringify an object it becomes  "[object Object]" so a["[object Object]"] = 123 and a["[object Object]"] = 456 , so when i log a[b] which is a["[object Object]"] , we see 456
