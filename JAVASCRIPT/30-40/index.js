c743f7e4e8ad59623a1a1d93ddc56b1b4914a94e
sagar <sagarc2580@gmail.com>
2024-01-17 16:26:59 +0530

    31

---
+++ JAVASCRIPT/30-40/index.js
@@ -0,0 +1,11 @@
+//!!31
+//todo
+// String.prototype.giveSagarPizza = () => {
+//   return "Just give sagar pizza already!";
+// };
+
+// const name = "sagar";
+
+// console.log(name.giveSagarPizza());
+
+//?? just give sagar pizza already , so here string is a built in function which i can add properties to , i just added a method to it's prototype priumitve stringsare automatically cvonverted into string object
